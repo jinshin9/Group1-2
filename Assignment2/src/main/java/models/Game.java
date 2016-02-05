a0e0829f4ad322d9688854ad1d4ff8acfefe29a6
braxtoncuneo <braxton.s.cuneo@gmail.com>
2016-02-04 21:06:06 -0800

    Added Detection and Reporting of Illegal Moves by User
    The program now prevents illegal moves requested by the player and
    displays a warning message regarding proper gameplay. This is done via
    the "errorCode" attribute of the game class and is displayed through the
    "errorCode" element of the page.

--- Assignment2/src/main/java/models/Game.java
+++ Assignment2/src/main/java/models/Game.java
@@ -13,46 +13,16 @@ public class Game {
 
     public java.util.List<java.util.List<Card>> cols = new ArrayList<>();
 
+    public String errorCode;
+
 
     public Game(){
         cols.add(new ArrayList<Card>());
         cols.add(new ArrayList<Card>());
         cols.add(new ArrayList<Card>());
         cols.add(new ArrayList<Card>());
+        errorCode=" ";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //I like space between my methods
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -65,42 +35,21 @@ public class Game {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-    //I like space between my methods
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public void shuffle() {
         long seed = System.nanoTime();
         Collections.shuffle(deck, new Random(seed));
     }
 
     public void dealFour() {
+        if(deck.isEmpty()){
+            errorCode="There are no more cards in the deck!";
+            return;
+        }
         for(int i = 0; i < 4; i++){
             cols.get(i).add(deck.get(deck.size()-1));
             deck.remove(deck.size()-1);
         }
+        errorCode=" ";
     }
 
     //customDeal to setup game for testing purposes
@@ -133,7 +82,15 @@ public class Game {
             }
             if (removeCard) {
                 this.cols.get(columnNumber).remove(this.cols.get(columnNumber).size() - 1);
+                errorCode=" ";
             }
+            else{
+                errorCode="That card is not valid to remove!";
+            }
+            return;
+        }
+        else{
+            errorCode="There is no card there to discard!";
         }
     }
 
@@ -150,9 +107,18 @@ public class Game {
 
 
     public void move(int colFrom, int colTo) {
+        if(cols.get(colFrom).size()==0){
+            errorCode="There is no card in the 'FROM' column to move!";
+            return;
+        }
+        if(cols.get(colTo).size()!=0){
+            errorCode="The 'TO' column is not empty!";
+            return;
+        }
         Card cardToMove = getTopCard(colFrom);
         this.removeCardFromCol(colFrom);
         this.addCardToCol(colTo,cardToMove);
+        errorCode=" ";
     }
 
     private void addCardToCol(int colTo, Card cardToMove) {
@@ -161,9 +127,5 @@ public class Game {
 
     private void removeCardFromCol(int colFrom) {
         this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
-
-        //I think that should conflict...
-        //Did it work yet?
-
     }
 }
